9051dbdac0dd6709e5a788c8b0b04ffc5437d3d2
Yas Afdal <80951059+yasafdal@users.noreply.github.com>
2026-09-24 01:35:13 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+#if DEBUG
+builder.Configuration.AddJsonFile("appsettings.Local.json", optional: true, reloadOnChange: true);
+#endif
+
+builder.CreateUmbracoBuilder()
+    .AddBackOffice()
+    .AddWebsite()
+    .AddComposers()
+    .Build();
+
+WebApplication app = builder.Build();
+
+
+await app.BootUmbracoAsync();
+
+app.UseHttpsRedirection();
+
+app.UseUmbraco()
+    .WithMiddleware(u =>
+    {
+        u.UseBackOffice();
+        u.UseWebsite();
+    })
+    .WithEndpoints(u =>
+    {
+        u.UseBackOfficeEndpoints();
+        u.UseWebsiteEndpoints();
+    });
+
+await app.RunAsync();
